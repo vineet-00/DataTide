5d88bfff130bb2665e372593b26cdfc9e2d69a00
Vineet <vineet.insights01@gmail.com>
2025-06-30 23:56:31 +0530

    added functionality to add and delete workflow cards

---
+++ app/(dashboard)/workflows/_components/DeleteWorkflowDialog.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { 
+  AlertDialog, 
+  AlertDialogAction, 
+  AlertDialogCancel, 
+  AlertDialogContent, 
+  AlertDialogDescription, 
+  AlertDialogFooter, 
+  AlertDialogHeader, 
+  AlertDialogTitle, 
+  AlertDialogTrigger
+ } from "@/components/ui/alert-dialog"
+ import { Input } from "@/components/ui/input"
+ import { useState } from "react"
+ import { useMutation } from "@tanstack/react-query"
+ import { DeleteWorkflow } from "@/actions/workflows/deleteWorkflow"
+ import { toast } from "sonner"
+
+ interface Props{
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  workflowName: string;
+  workflowId: string;
+ }
+
+ export const DeleteWorkflowDialog = ({open, setOpen, workflowName, workflowId}: Props) => {
+
+    const [confirmText, setConfirmText] = useState("")
+    const deleteMutation= useMutation({
+      mutationFn: DeleteWorkflow,
+      onSuccess: () => {
+        toast.success("Workflow deleted successfully", {id: workflowId})
+        setConfirmText("")
+      },
+      onError: () => {
+        toast.error("Something went wrong", {id: workflowId})
+      },
+    })
+
+   return (
+     <AlertDialog open={open} onOpenChange={setOpen}>
+       <AlertDialogContent>
+         <AlertDialogHeader>
+           <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+           <AlertDialogDescription>
+            If you delete this workflow, you will not be able to recove it.
+            <div className="flex flex-col py-4 gap-2">
+              <p>If you are sure, enter <b>{workflowName}</b> to confirm:</p>
+              <Input 
+                value={confirmText}
+                onChange={(e) => setConfirmText(e.target.value)}
+              /> 
+            </div>
+           </AlertDialogDescription>
+         </AlertDialogHeader>
+         <AlertDialogFooter>
+           <AlertDialogCancel onClick={() => setConfirmText("")}>Cancel</AlertDialogCancel>
+           <AlertDialogAction 
+            disabled={confirmText !== workflowName || deleteMutation.isPending}
+            className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+            onClick = {(e) => {
+              // e.stopPropagation()
+              toast.loading("Deleting workflow...", {id: workflowId})
+              deleteMutation.mutate(workflowId)
+            }}
+           >Delete</AlertDialogAction>
+         </AlertDialogFooter>
+       </AlertDialogContent>
+     </AlertDialog>
+   )
+ }
+
+ export default DeleteWorkflowDialog
+ 
---
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -0,0 +1,102 @@
+"use client"
+
+import { Workflow } from "@prisma/client"
+import { Card, CardContent } from "@/components/ui/card"
+import { WorkflowStatus } from "@/types/workflow"
+import { FileTextIcon, PlayIcon, ShuffleIcon, MoreVerticalIcon, TrashIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuItem, DropdownMenuLabel, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import TooltipWrapper from "@/components/TooltipWrapper"
+import { useState } from "react"
+import DeleteWorkflowDialog from "@/app/(dashboard)/workflows/_components/DeleteWorkflowDialog"
+
+
+const statusColors ={
+  [WorkflowStatus.DRAFT] : "bg-yellow-400 text-yellow-600",
+  [WorkflowStatus.PUBLISHED] : "bg-primary",
+}
+
+export const WorkflowCard = ({workflow}: {workflow: Workflow}) => {
+  
+  const isDraft = workflow.status === WorkflowStatus.DRAFT
+
+  return <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primary/30">
+    <CardContent className="p-4 flex items-center justify-between h-[100px]">
+      <div className="flex items-center justify-end space-x-3">
+        <div className={cn("w-10 h-10 rounded-full flex items-center justify-center", statusColors[workflow.status as WorkflowStatus])}>
+          {isDraft? (<FileTextIcon className="h-5 w-5" />) : (<PlayIcon className="h-5 w-5 text-white" />) }
+        </div>
+        <div>
+          <h3 className="text-base font-bold text-muted-foreground flex items-center">
+            <Link href={`/workflow/editor/${workflow.id}`} className="flex items-center hover:underline">
+              {workflow.name}
+            </Link>
+            {isDraft && (
+              <span className="ml-2 px-2 py-0.5 text-xs font-medium bg-yellow-100 text-yellow-800 rounded-full">
+                Draft
+              </span>
+            )}
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <Link href={`/workflow/editor/${workflow.id}`} 
+          className={cn(buttonVariants({
+            variant: "outline",
+            size: "sm",
+          }),
+          "flex items-center gap-2"
+          )}
+        >
+          <ShuffleIcon size={16}/>
+          Edit      
+        </Link>
+        <WorkflowActions workflowName={workflow.name} workflowId={workflow.id}/>
+      </div>
+    </CardContent>
+  </Card>
+}
+
+function WorkflowActions ({workflowName, workflowId}: {workflowId: string, workflowName: string}) {
+
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false)
+
+  return (
+    <>
+      <DeleteWorkflowDialog 
+        open={showDeleteDialog} 
+        setOpen={setShowDeleteDialog} 
+        workflowName = {workflowName}
+        workflowId= {workflowId}
+      /> 
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"outline"} size={"sm"}>
+            <TooltipWrapper content={"More actions"}>
+              <div className="flex items-center justify-center w-full h-full">
+                <MoreVerticalIcon size={18} />
+              </div>
+            </TooltipWrapper>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem 
+            className="text-destructive flex items-center gap-2"
+            onSelect ={() => {
+              setShowDeleteDialog((prev) => !prev);
+            }}
+          >
+            <TrashIcon size={16} /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
+
+export default WorkflowCard
